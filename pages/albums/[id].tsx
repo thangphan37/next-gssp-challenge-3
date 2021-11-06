bfab194fafbffdf928b52f6116bfa4f46219be32
thangphan37 <pcthangk59@gmail.com>
2021-11-06 15:10:28 +0900

    add

---
+++ pages/albums/[id].tsx
@@ -0,0 +1,14 @@
+/**
+ * This is a free public API: https://jsonplaceholder.typicode.com/albumns/1
+ * Requirements:
+ * 	- Pre-render data of album at the request time by id.
+ * Hint:
+ * 	- Using `params` value in the `getServerSideProps` function.
+ * 	- Using `getServerSideProps`.
+ */
+
+function Album() {
+  return <div>{/* add your code here */}</div>
+}
+
+export default Album
